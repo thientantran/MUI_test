97bb6d33053e7498637c0678410d5de12d9503e1
thientantran <thientantm@gmail.com>
2023-07-03 21:30:57 +0700

    setting project
    fixup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,54 @@
 module.exports = {
-  env: { browser: true, es2020: true },
   extends: [
+    // Chúng ta sẽ dùng các rule mặc định từ các plugin mà chúng ta đã cài.
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    // Disable các rule mà eslint xung đột với prettier.
+    // Để cái này ở dưới để nó override các rule phía trên!.
+    'eslint-config-prettier',
+    'prettier'
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  plugins: ['prettier'],
+  settings: {
+    react: {
+      // Nói eslint-plugin-react tự động biết version của React.
+      version: 'detect'
+    }
   },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  rules: {
+    // Tắt rule yêu cầu import React trong file jsx
+    'react/react-in-jsx-scope': 'off',
+    // Cảnh báo khi thẻ <a target='_blank'> mà không có rel="noreferrer"
+    'react/jsx-no-target-blank': 'warn',
+    // Tăng cường một số rule prettier (copy từ file .prettierrc qua)
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        semi: false,
+        trailingComma: 'none',
+        tabWidth: 2,
+        endOfLine: 'auto',
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true
+      }
+    ]
+  }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import path from 'path'
+import { defineConfig } from 'vite'
+import eslintPlugin from 'vite-plugin-eslint'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      cache: false,
+      include: ['./src/**/*.js', './src/**/*.jsx'],
+      exclude: []
+    })
+  ],
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
+    }
+  }
 })
