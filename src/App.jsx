f94e313e0087d79c34271ae3cb75407e0d87dde2
thientantran <thientantm@gmail.com>
2023-07-03 22:14:41 +0700

    basic of MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import { Button } from '@mui/material'
+import { Add, Settings } from '@mui/icons-material'
+import { Button, Typography } from '@mui/material'
 import './App.css'
-
 function App() {
   return (
     <div>
       <Button variant='text'>Text</Button>
-      <Button variant='contained'>Contained</Button>
-      <Button variant='outlined'>Outlined</Button>
+      <Button variant='contained' color='secondary' size='small' startIcon={<Settings />}>
+        Contained
+      </Button>
+      <Button variant='contained' size='small' sx={{ marginLeft: '10px' }} color='success' startIcon={<Add />}>
+        Add
+      </Button>
+      <Typography variant='h1' component='h2'>
+        h1. Heading
+      </Typography>
+      <Button
+        variant='contained'
+        sx={{
+          backgroundColor: 'skyblue',
+          color: 'black',
+          margin: 5,
+          '&:hover': {
+            backgroundColor: 'lightblue'
+          },
+          '&:disabled': {
+            backgroundColor: 'gray',
+            color: 'white'
+          }
+        }}
+      >
+        My Unique Button
+      </Button>
     </div>
   )
 }
