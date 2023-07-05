79cdbdfb4a823b266c04ff371f1c6c8383a95e37
thientantran <thientantm@gmail.com>
2023-07-05 09:26:44 +0700

    Responsive

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
 
 export default function Rightbar() {
-  return <Box flex={2} p={2} sx={{backgroundColor: 'lightcoral'}}>Rightbar</Box>
+  return <Box flex={2} p={2} sx={{backgroundColor: 'lightcoral',display:{xs:"none", sm:"block"}}}>Rightbar</Box>
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,6 @@ import { Box } from '@mui/material'
 import React from 'react'
 export default function Sidebar() {
   return (
-    <Box flex={1} p={2} sx={{backgroundColor:"skyblue"}}>Sidebar</Box>
+    <Box flex={1} p={2} sx={{backgroundColor:"skyblue", display:{xs:"none", sm:"block"}}}>Sidebar</Box>
   )
 }
